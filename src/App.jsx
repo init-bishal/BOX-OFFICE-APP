0273a96e49d71a7b0bb4eddee54e16de4e4ef488
Bishal Ghosh <bbishal.g01@gmail.com>
2023-07-28 10:34:19 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import {BrowserRouter as Router , Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 function App() {
   return (
-    <div >
-       Hello World
-    </div>
+    <Router>
+      <Routes>
+           <Route path="/" element={<Home/>}/>
+           <Route path="/starred" element={<Starred/>}/>
+           <Route path="*" element={<div>Not Found</div>}/>
+
+      </Routes>
+    </Router>
   );
 }
 
